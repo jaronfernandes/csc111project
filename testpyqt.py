6e21c5b7557df34e2c66d4fefda0bd7a93fb5ecc
WarfighterXK <74974138+WarfighterXK@users.noreply.github.com>
2023-03-26 23:08:49 -0400

    Add files via upload
    Updated ver

--- testpyqt.py
+++ testpyqt.py
@@ -1,53 +1,19 @@
-"""test qt"""
-import json
+"""test qt
 
-import PyQt6
+jaiz if you see this im sorry for the code being so messy and undocumented lol
+
+i will fix later
+"""
+from __future__ import annotations
+
+import json
 
 import sys
 
 from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import *
 from PyQt6.QtGui import QFont, QFontDatabase
-from PyQt6.QtWidgets import (
-    QApplication,
-    QCheckBox,
-    QCompleter,
-    QComboBox,
-    QDateEdit,
-    QDateTimeEdit,
-    QDial,
-    QDoubleSpinBox,
-    QFontComboBox,
-    QLabel,
-    QLCDNumber,
-    QLineEdit,
-    QMainWindow,
-    QProgressBar,
-    QPushButton,
-    QSpacerItem,
-    QSizePolicy,
-    QRadioButton,
-    QSlider,
-    QSpinBox,
-    QTimeEdit,
-    QHBoxLayout,
-    QVBoxLayout,
-    QWidget,
-)
-
-
-class OnOffWidget(QWidget):
-    """deez"""
-    def __init__(self, name):
-        super(OnOffWidget, self).__init__()
-
-        self.name = name  # Name of widget used for searching.
-        self.label = QLabel(self.name)
-        self.remove_button = QPushButton('X')
-
-        self.hbox = QHBoxLayout()  # A horizontal layout to encapsulate the above
-        self.hbox.addWidget(self.label)  # Add the label to the layout
-        self.hbox.addWidget(self.remove_button)
-        self.setLayout(self.hbox)
+from PyQt6 import QtCore
 
 
 def extract_movies_file(filename: str) -> set:
@@ -62,69 +28,120 @@ def extract_movies_file(filename: str) -> set:
     return names
 
 
-# Subclass QMainWindow to customize your application's main window
+class MovieWidget(QWidget):
+    """Widget for each movie in the dropdown."""
+    movie_name: str
+    label: QLabel
+    close_button: QPushButton
+    layout: QHBoxLayout
+    parent: MainWindow
+
+    def __init__(self, movie_name: str, parent: MainWindow):
+        """Initializer"""
+        super(MovieWidget, self).__init__()
+        self.parent = parent
+
+        # self.setMaximumHeight(50)
+        self.movie_name = movie_name
+        self.label = QLabel(self.movie_name, self)
+        self.close_button = QPushButton('X', self)
+        self.close_button.setFixedSize(QtCore.QSize(40, 40))
+        self.close_button.setFont(QFont('Verdana', 20))
+
+        self.layout = QHBoxLayout()
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.close_button)
+        self.setLayout(self.layout)
+
+        batotn = self.close_button
+        batotn.clicked.connect(self.on_clicked)
+
+    def on_clicked(self) -> None:
+        """When the button is clicked."""
+        self.parent.added_movies.remove(self.movie_name)
+        self.layout.removeWidget(self)
+        # self.destroy()
+
+
 class MainWindow(QMainWindow):
-    """
-        we
-    """
+    """Main window for the application."""
+    add_movie_button: QPushButton
+    added_movies: set
+    completer: QCompleter
+    container: QWidget
+    container_layout: QVBoxLayout
+    form_layout: QFormLayout
+    scroll: QScrollArea
     searchbar: QLineEdit
+    movies: set
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
 
-        self.setWindowTitle("Anime Recommendation System")
-        self.controls = QWidget()
-        self.controlsLayout = QVBoxLayout()
+        self.setWindowTitle('Anime Recommendation System')
+        self.container = QWidget()
+        self.container_layout = QVBoxLayout()
         self.searchbar = QLineEdit()
-        self.button_push = QPushButton()
+        self.add_movie_button = QPushButton('Add Movie')
+        self.form_layout = QFormLayout()
+        self.added_movies = set()
 
-        layout = QVBoxLayout()
-        widgets = [
-            QCheckBox,
-            QLabel,
-        ]
+        movie_names = extract_movies_file('datasets/filtered/final_imdb_movies.json')
+        movie_names.union(extract_movies_file('datasets/filtered/final_imdb_shows.json'))
 
-        self.widgets = []
+        self.movies = movie_names
 
-        movie_names = extract_movies_file('datasets/filtered/final_movies.json')
+        # To space the drop-down.
+        spacer = QSpacerItem(0, 0, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+        self.container_layout.addItem(spacer)
+        self.container.setLayout(self.container_layout)
 
-        for name in movie_names:
-            item = OnOffWidget(name)
-            self.controlsLayout.addWidget(item)
-            self.widgets.append(item)
-
-        spacer = QSpacerItem(1, 1, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
-        self.controlsLayout.addItem(spacer)
-        self.controls.setLayout(self.controlsLayout)
-
-        widget = QLabel("Enter a list of movies you've watched below!")
-        widget.setFont(QFont('Verdana', 20))
-        widget.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
-        layout.addWidget(widget)
-
-        layout.addWidget(self.searchbar)
-
-        for w in widgets:
-            if w is not QCompleter:
-                layout.addWidget(w())
-
-        layout.addWidget(self.button_push)
-
-        widget = QWidget()
-        widget.setLayout(layout)
-
-        # Adding Completer.
+        # Create the auto-complete
         self.completer = QCompleter(movie_names)
         self.completer.setCaseSensitivity(Qt.CaseSensitivity.CaseInsensitive)
         self.searchbar.setCompleter(self.completer)
 
-        # Set the central widget of the Window. Widget will expand
-        # to take up all the space in the window by default.
-        self.setCentralWidget(widget)
+        # Group Box
+        group_box = QGroupBox('Movies Added')
+        group_box.setFont(QFont('Callibri', 15))
+
+        group_box.setLayout(self.form_layout)
+
+        # Scroll Area Properties.
+        self.scroll = QScrollArea()
+        self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
+        self.scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.scroll.setWidgetResizable(True)
+        self.scroll.setWidget(group_box)
+
+        # Creating the container
+        container = QWidget()
+        container_layout = QVBoxLayout()
+        row = QFormLayout()
+        row.addRow(self.searchbar, self.add_movie_button)
+        container_layout.addLayout(row)
+        thing = QVBoxLayout()
+
+        self.scroll.setLayout(thing)
+        container_layout.addWidget(self.scroll)
+
+        # for movie in self.movies:
+        #     container_layout.addWidget(movie)
+
+        container.setLayout(container_layout)
+        self.setCentralWidget(container)
+
+        self.add_movie_button.clicked.connect(self.on_movie_added)
+
+    def on_movie_added(self) -> None:
+        """Button for when movies are added"""
+        text = self.searchbar.text()
+        if text in self.movies and text not in self.added_movies:
+            self.form_layout.addRow(MovieWidget(text, self))
+            self.added_movies.add(text)
 
 
 app = QApplication(sys.argv)
-window = MainWindow()
-window.show()
-
-app.exec()
+w = MainWindow()
+w.show()
+sys.exit(app.exec())
